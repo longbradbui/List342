58a1f1adaa46d02b9b791ddb1e8370d7fc757b91
Long Bui <bhoanglong15020@gmail.com>
2024-02-09 13:28:15 -0800

    Implement Constructors

--- list342.h
+++ list342.h
@@ -4,15 +4,16 @@
 #include <vector>
 using namespace std;
 
+/* CONTRACT DECLARATION */
 /* Node Struct */
 template<class T>
 struct Node
 {
-	T* data;
-	Node* next;
+	T* data;     // A pointer to the data value
+	Node* next; //  A pointer to the next Node 
 }; 
 
-/* List342 Class*/
+/* List342 Class */
 template <class T>
 class List342
 {
@@ -20,9 +21,8 @@ public:
 	/* CONSTRUCTOR and DESTRUCTOR */
 	List342();                                  // Default Constructor
 	List342(string file_name);                 //  Parameterized Contructor: Read from file
-	List342(List342<T>& input_list);          //   Parameterized Constructor: Read from class
-	List342(const List342<T>& parent_list);  //    Copy Contructor
-	~List342();                             //     Default Destructor
+	List342(const List342<T>& parent_list);   //   Copy Contructor
+	~List342();                              //    Default Destructor
 	/* ACTIONS */
 	bool BuildList(string file_name);
 	bool Insert(T* obj);
@@ -33,15 +33,40 @@ public:
 	int Size() const;
 	/* OPERATOR OVERLOADING */
 	friend ostream& operator<<(ostream& outstream, const List342<T> rhs_list);
-	List342<T>& operator=(const List342<T>& rhs_list);
-	bool operator==(const List342<T>& rhs_list);
-	bool operator!=(const List342<T>& rhs_list);
-	List342<T>& operator+=(const List342<T>& rhs_list);
-	List342<T> operator+(const List342<T>& rhs_list) const;
+	List342<T>& operator=(const List342<T>& rhs_list);           // Performs Deep Copy of two sorted lists
+	bool operator==(const List342<T>& rhs_list);                //  Equality comparison
+	bool operator!=(const List342<T>& rhs_list);               //   Inequality comparison
+	List342<T>& operator+=(const List342<T>& rhs_list);       //    Returns a reference to the lhs list (sorted)
+	List342<T> operator+(const List342<T>& rhs_list) const;  //     Returns a new list (sorted)
 private:
 	Node<T>* head_ptr_;
-
-
 };
 #endif
+
+/* CONSTRUCTOR IMPLEMENTATION */
+template <class T>
+List342<T>::List342()
+{
+	this->head_ptr_ = nullptr;
+}
+
+template <class T>
+List342<T>::List342(const List342<T> & input_list)
+{
+	this->head_ptr_ = nullptr;
+	*this = input_list;
+}
+
+template <class T>
+List342<T>::List342(string file_name)
+{
+	BuildList(file_name);
+};
+
+template <class T>
+List342<T>::~List342()
+{
+	DeleteList();
+}
+
 
