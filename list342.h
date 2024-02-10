db94658c28a68cc0c4450adcf5dedea6e08098c2
Long Bui <bhoanglong15020@gmail.com>
2024-02-09 18:42:37 -0800

    Code Commenting

--- list342.h
+++ list342.h
@@ -81,16 +81,19 @@ bool List342<T>::BuildList(string file_name)
 template <class T>
 bool List342<T>::Insert(T* obj)
 {
+    // If object is pointing to null reference
     if (obj == nullptr)
     {
         return false;
     }
+    // If head pointer is pointing to null reference
     if (head_ptr_ == nullptr)
     {
         head_ptr_ = new Node();
         head_ptr_->data = new T(*obj);
         return true;
     }
+    // If value pointed by object is less than value pointed by head pointer
     Node* insert_node = new Node();
     insert_node->data = new T(*obj);
     if (*insert_node->data < *head_ptr_->data)
@@ -99,13 +102,18 @@ bool List342<T>::Insert(T* obj)
         head_ptr_ = insert_node;
         return true;
     }
+    // Essentially, current node is pointing to head pointer
+    // and previous node is pointing to null reference
     Node* current_node = head_ptr_;
     Node* previous_node = nullptr;
+    // While: current node is not pointing to null reference 
+    // and value of current node is less than value of pointer object 
     while ((current_node->next != nullptr) && (*current_node->data < *insert_node->data))
     {
-        previous_node = current_node;
-        current_node = current_node->next;
-    }
+        previous_node = current_node; // previous node points to what current node is pointing
+        current_node = current_node->next; // current node points to next node
+    }// exits out the loop if one of two conditions fail
+    // Check for duplication
     if (previous_node != nullptr && *previous_node->data == *insert_node->data)
     {
         delete insert_node;
@@ -116,8 +124,6 @@ bool List342<T>::Insert(T* obj)
     {
         previous_node->next = insert_node;
     }
+
     return true;
 }
-
-
-
